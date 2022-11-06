18ae767c4d7770a0dc99180d68dd6ca7e51f4821
graham <graham.annett@gmail.com>
2022-11-05 22:03:19 -0600

    have to figure out plan for screenshots probably

--- weblm/crawler.py
+++ weblm/crawler.py
@@ -2,13 +2,13 @@ import asyncio
 import json
 import re
 import time
+import uuid
+from dataclasses import dataclass, field
 from os.path import exists
 from sys import platform
-from typing import List
+from typing import Any, List
 
-
-from playwright.async_api import async_playwright
-from playwright.sync_api import sync_playwright, BrowserContext
+from playwright.sync_api import Page, sync_playwright
 
 from .parser import Parser, TasksInterface
 
@@ -42,6 +42,42 @@ def replace_special_fields(cmd):
             cmd = cmd.replace(k, v)
 
     return cmd
+
+
+@dataclass
+class Element:
+    node_index: str
+    backend_node_id: int
+    node_name: str
+    node_value: Any
+    is_clickable: bool
+
+    origin_x: int
+    origin_y: int
+    center_x: int
+    center_y: int
+
+    node_meta: List[str] = field(default_factory=list)
+
+
+class ElementCapture:
+    def __init__(self):
+        self.folder = "screenshots"
+
+    def capture(self, page: Page, element: Element):
+        screenshot = page.screenshot(
+            clip={
+                "x": element.origin_x,
+                "y": element.origin_y,
+                "width": element.center_x,
+                "height": element.origin_y,
+            },
+            path=self.latest_file(),
+        )
+        return screenshot
+
+    def latest_file(self):
+        return f"{self.folder}/{str(uuid.uuid4())}.png"
 
 
 class Crawler:
